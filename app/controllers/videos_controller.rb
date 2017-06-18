3e9a285e2b7abbfc58014dda13023664cb7b18c5
Rahul <rahulpatil2387@gmail.com>
2017-06-19 05:13:00 +0530

    Configure yt gem to use and retrive data

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,11 @@
 class VideosController < ApplicationController
-  def index
-  end
+	def index
+		if params[:url].present?
+			@video = Yt::Video.new url: params[:url]
+		end
+		respond_to do |format|
+			format.html
+			format.js
+		end
+	end
 end
